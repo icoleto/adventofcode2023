60762c72c0371c933e00f5cd416e371bbd268d3a
Iván Coleto <ivan.coleto@repsol.com>
2023-12-04 16:09:39 +0100

    update with day2 first test

--- day2.ts
+++ day2.ts
@@ -1,30 +1,47 @@
 import * as fs from "fs";
+import { parse } from "path";
 
-const input = fs.readFileSync("./day1input.txt", "utf8").split("\n");
-
-function getFirstNumber(val: string): string {
-  return val.match(/\d/)?.[0] || "0";
+const input = fs.readFileSync("./day2input.txt", "utf8").split("\n");
+class Game {
+  id: number;
+  plays: Play[];
+}
+function parseLine(line: string): Game {
+  let game = {} as any;
+  line = line.replace("Game ", "");
+  game.id = Number(line.match(/\d+/)[0]);
+  line = line.replace(/\d+: /, "");
+  game.plays = line.split(";").map(Play.parsePlay);
+  return game;
+}
+class Play {
+  blue: number;
+  red: number;
+  green: number;
+  static parsePlay(play: string): Play {
+    const matchedBlue = play.match(/\d+ blue/);
+    const matchedRed = play.match(/\d+ red/);
+    const matchedGreen = play.match(/\d+ green/);
+    return {
+      blue: Number(matchedBlue && matchedBlue[0].replace(" blue", "")),
+      red: Number(matchedRed && matchedRed[0].replace(" red", "")),
+      green: Number(matchedGreen && matchedGreen[0].replace(" green", "")),
+    };
+  }
 }
 
-function getLastNumber(val: string): string {
-  const matches = val.match(/\d/g);
-  return matches?.[matches.length - 1] || "0";
-}
+let gamesIdAcc = 0;
+const initialSetup = { blue: 14, red: 12, green: 13 };
+const games = input.map(parseLine);
+games.forEach((game) => {
+  game.plays.every(
+    (play) =>
+      play.blue <= initialSetup.blue &&
+      play.green <= initialSetup.green &&
+      play.red <= initialSetup.red
+  )
+    ? (gamesIdAcc += game.id)
+    : null;
+});
 
-function getNumberComposedByTwoNumericChars(
-  first: string,
-  second: string
-): number {
-  return Number(first + second);
-}
-
-const total = input.reduce((acc: number, val: string) => {
-  const firstNumber = getFirstNumber(val);
-  const lastNumber = getLastNumber(val);
-  const composedNumber = getNumberComposedByTwoNumericChars(
-    firstNumber,
-    lastNumber
-  );
-  return acc + composedNumber;
-}, 0);
-console.log(total);
+console.log(gamesIdAcc);
