4e475c724e7192b8423611dfcb54196f278ae123
Iván Coleto <ivancoleto@gmail.com>
2023-12-06 11:15:27 +0100

    update with day5-2.ts

---
+++ day5-2.ts
@@ -0,0 +1,73 @@
+import * as fs from "fs";
+const input = fs.readFileSync("./day5input.txt", "utf8");
+
+function getMap(regex: RegExp) {
+  const rules: { from: number; to: number; length: number }[] = [];
+  const parsedTextMap: string[][] = input
+    .match(regex)[0]
+    .match(/\d* \d* \d*\n/g)
+    .map((a) => a.replace("\n", ""))
+    .map((a) => a.split(" "));
+
+  for (let line of parsedTextMap) {
+    const [t, f, l] = line;
+    const to = Number(t);
+    const from = Number(f);
+    const length = Number(l);
+    rules.push({ to, from, length });
+  }
+
+  return (number) => {
+    for (let rule of rules) {
+      if (number >= rule.from && number <= rule.from + rule.length) {
+        return rule.to + (number - rule.from);
+      }
+    }
+    return null;
+  };
+}
+
+const seed_to_soilMap = getMap(/seed-to-soil map:\n[\d \n]*/);
+const soil_to_fertilizerMap = getMap(/soil-to-fertilizer map:\n[\d \n]*/);
+const fertilizer_to_waterMap = getMap(/fertilizer-to-water map:\n[\d \n]*/);
+const water_to_lightMap = getMap(/water-to-light map:\n[\d \n]*/);
+const light_to_temperatureMap = getMap(/light-to-temperature map:\n[\d \n]*/);
+const temperature_to_humidityMap = getMap(
+  /temperature-to-humidity map:\n[\d \n]*/
+);
+const humidity_to_locationMap = getMap(/humidity-to-location map:\n[\d \n]*/);
+
+const allMaps: Function[] = [
+  seed_to_soilMap,
+  soil_to_fertilizerMap,
+  fertilizer_to_waterMap,
+  water_to_lightMap,
+  light_to_temperatureMap,
+  temperature_to_humidityMap,
+  humidity_to_locationMap,
+];
+
+let minSeedToLocationValue = null;
+
+const pairs = input
+  .match(/seeds:[^\n]*\n/)[0]
+  .match(/\d+ \d+/g)
+  .map((val) => val.split(" "));
+
+for (let i = 0; i < pairs.length; i++) {
+  const pair = pairs[i];
+  console.log(`${i + 1}/${pairs.length} started`);
+  for (let i = 0; i < Number(pair[1]); i++) {
+    let seed = Number(pair[0]) + i;
+    let value = Number(seed);
+    for (let map of allMaps) {
+      value = map(Number(value)) || value;
+    }
+    if (value < minSeedToLocationValue || !minSeedToLocationValue) {
+      minSeedToLocationValue = value;
+    }
+  }
+  console.log(`${i + 1}/${pairs.length} finished`);
+}
+
+console.log(`minSeedToLocationValue: ${minSeedToLocationValue}`);
